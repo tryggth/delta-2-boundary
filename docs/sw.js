d1d30cb47e370c317bdab9970bbdf9f24056e9ae
tryggth <tryggth2009@gmail.com>
2026-08-04 15:58:52 -0700

    Add Hidden Mode boundary vertex coordinate & turn angle debug hover tooltip

--- docs/sw.js
+++ docs/sw.js
@@ -1,5 +1,5 @@
 // Spectre Monotile Δ₂ PWA Service Worker (Auto-Updating)
-const CACHE_NAME = 'spectre-puzzle-pwa-v1.0.1785883695188';
+const CACHE_NAME = 'spectre-puzzle-pwa-v1.0.1785884332909';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
